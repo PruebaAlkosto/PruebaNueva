9c12d037e62decb0cf182fb411097f242f05f8f1
PruebaAlkosto <pruebasalkosto2@gmail.com>
2025-05-23 11:24:16 -0500

    valentina prueba

---
+++ CursoEcommerce/src/test/java/stepdefinition/Nuevo3.java
@@ -0,0 +1,34 @@
+package stepdefinition;
+
+import io.cucumber.java.es.Cuando;
+import io.cucumber.java.es.Dado;
+import io.cucumber.java.es.Entonces;
+import net.thucydides.core.annotations.Step;
+import task.LoginPage;
+
+public class Nuevo3 {
+
+    LoginPage pageLogin;
+
+
+    @Dado("El usuario quiere loguearse")
+    public void elUsuarioQuiereLoguearse() {
+        pageLogin.open();
+
+    }
+
+    @Cuando("da click en continuar")
+    public void daClickEnContinuar(String username, String pass) {
+        pageLogin.Login(username, pass);
+    }
+
+    @Entonces("ingresa al portal")
+    public void ingresaAlPortal() {
+        throw new io.cucumber.java.PendingException();
+    }
+    @Step
+    public void LoginPortal(String username, String pass) {
+        pageLogin.open();
+        pageLogin.Login(username, pass);
+    }
+}
